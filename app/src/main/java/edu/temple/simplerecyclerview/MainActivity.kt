bb56919ed1d4615296b5d941457455ff56032349
Victoria <tovictoriadao@gmail.com>
2024-09-24 15:15:18 -0400

    Worked on class assignment with group. Created a recycler view where each textview displays each number in an integer array and the number also corresponds to its text size.

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +10,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO Step 1: Reference RecyclerView object
+        // Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        //TODO Step 2: Provide a LayoutManager
-
+        // Step 2: Provide a LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+        val numbers = IntArray(25){(it+1) * 5}
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
+
+
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,45 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
+//Step 3a: Provide Adapter Parent
+//Extend to the RecyclerView.Adapter and place the current's class ViewHolder
+class NumberDisplayAdapter(private val numbers: IntArray) :
+    RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+    //List all the elements of the view (each component such as images, buttons, etc.)
+    //In this case, this holder only holds one type of view: TextView
+    class NumberViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView) {}
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    // Step 3b: Complete function definitions for adapter
+    //Create Views (For example, setting how big each TextView is)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        NumberViewHolder(TextView(parent.context).apply {
+            setPadding(3, 4, 3, 4)
+        }
+        )
 
-    // TODO Step 3b: Complete function definitions for adapter
+    //Alternative
+//        val textView = TextView(parent.context)
+//        textView.setPadding(3, 4, 3,4)
+//        val numberViewHolder = NumberViewHolder(textView)
+//        return numberViewHolder
+
+    //Populating views (color, images, text, size)
+    //Here for each TextView view, we added the text and then changed the size
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textView.text = numbers[position].toString()
+        holder.textView.textSize = numbers[position].toFloat()
+    }
+
+    //Return the total number of views in this holder (Int)
+    //An expression can be evaluated and returns a value
+    // Ex. return 3-1 => An expression that always evaluate to 2
+    override fun getItemCount() = numbers.size
+
 
 }
